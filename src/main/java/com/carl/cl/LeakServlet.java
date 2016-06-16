e6b054efb0f5aac3ee89a8e4c6098e8a88c1ea1f
Carl Tien <carltien@mail.com>
2016-06-16 23:34:30 +0800

    Update LeakServlet.java

--- src/main/java/com/carl/cl/LeakServlet.java
+++ src/main/java/com/carl/cl/LeakServlet.java
@@ -24,21 +24,34 @@ public class LeakServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         String pageNum = req.getParameter("page");
-        List<String> picList = new ArrayList<String>();
-	String baseurl = "http://cc.8sss.me/";
-	Document doc = Jsoup.connect(baseurl + "thread0806.php").data("fid", "16").data("page", pageNum)
-		.userAgent("Mozilla").get();
-	Element div = doc.getElementById("ajaxtable");
-	Elements d = div.select("tbody:nth-child(2) > tr > td >h3").select("a[href]");
-	for (Element b : d) {
-		Document detailPage = Jsoup.connect(baseurl + b.attr("href")).userAgent("Mozilla").get();
-		Elements p = detailPage.select("div > input");
-		for (Element c : p) {
-			if (!c.attr("src").equals("")) {
-				picList.add(c.attr("src"));
+       List<String> picList = new ArrayList<String>();
+		String baseurl = "http://cc.8sss.me/";
+		Document doc=null;
+		try {
+			doc = Jsoup.connect(baseurl + "thread0806.php").data("fid", "16").data("page", pageNum)
+					.userAgent("Mozilla").get();
+		} catch (IOException e1) {
+			ServletOutputStream out = resp.getOutputStream();
+	        out.write((new Gson()).toJson(picList).getBytes());
+	        out.flush();
+	        out.close();
+		}
+		Element div = doc.getElementById("ajaxtable");
+		Elements d = div.select("tbody:nth-child(2) > tr > td >h3").select("a[href]");
+		for (Element b : d) {
+			Document detailPage = null;
+			try {
+				detailPage = Jsoup.connect(baseurl + b.attr("href")).userAgent("Mozilla").get();
+			} catch (IOException e) {
+				continue;
+			}
+			Elements p = detailPage.select("div > input");
+			for (Element c : p) {
+				if (!c.attr("src").equals("")) {
+					picList.add(c.attr("src"));
+				}
 			}
 		}
-	}
         ServletOutputStream out = resp.getOutputStream();
         out.write((new Gson()).toJson(picList).getBytes());
         out.flush();
