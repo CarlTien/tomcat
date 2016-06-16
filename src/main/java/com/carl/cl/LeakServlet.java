fd262aacaf7dce50cf22699b4baef4944a51e439
Carl Tien <carltien@mail.com>
2016-06-16 22:55:28 +0800

    Create LeakServlet.java

---
+++ src/main/java/com/carl/cl/LeakServlet.java
@@ -0,0 +1,27 @@
+package com.carl.cl;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+        name = "CaoServlet", 
+        urlPatterns = {"/cao"}
+    )
+public class LeakServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        out.write("草1024".getBytes());
+        out.flush();
+        out.close();
+    }
+    
+}
